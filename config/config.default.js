56edcdac3d1887c2f400477c8dc313512c0a484d
bourne <xuhui.yon@gmail.com>
2023-11-27 11:27:17 +0800

    pause

--- config/config.default.js
+++ config/config.default.js
@@ -23,26 +23,6 @@ module.exports = appInfo => {
       enable: false,
     },
   };
-  config.mysql = {
-    // 单数据库信息配置
-    client: {
-      // host
-      host: 'mariadb',
-      // host: '124.220.108.159',
-      // 端口号
-      port: '3306',
-      // 用户名
-      user: 'root',
-      // 密码
-      password: 'mariadb2Root3PWD6',
-      // 数据库名
-      database: 'base_db',
-    },
-    // 是否加载到 app 上，默认开启
-    app: true,
-    // 是否加载到 agent 上，默认关闭
-    agent: false,
-  };
 
   // add your user config here
   const userConfig = {
